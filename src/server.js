fb383dedc5c57eab47c5029071e1c75e43520cfc
dayosama0 <dedinside02@mail.ru>
2026-02-09 23:48:19 +0500

    Update API logic

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 require("dotenv").config();
 
+const express = require("express");
+const path = require("path");
+
 const app = require("./app");
 const connectDB = require("./config/db");
 
@@ -10,11 +13,20 @@ if (!MONGODB_URI) {
   console.error("Missing MONGODB_URI in .env");
   process.exit(1);
 }
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 (async () => {
   try {
     await connectDB(MONGODB_URI);
-    app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+
+    app.listen(PORT, "0.0.0.0", () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+
   } catch (err) {
     console.error("Failed to start server:", err);
     process.exit(1);
